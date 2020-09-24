9deadc27200990416b00d9a7c91dd076a62e542f
Tony <jatimberlake1@crimson.ua.edu>
2020-09-24 13:31:29 -0500

    Had misread the problem prior, just getting one integer value to work on

--- CodeChallenge/src/Discover10Friendly.java
+++ CodeChallenge/src/Discover10Friendly.java
@@ -8,26 +8,27 @@ public class Discover10Friendly {
 	{
 		while (true)
 		{
-			System.out.println(Arrays.toString(readIO()));
+//			System.out.println(Arrays.toString(readIO()));
+			int upperBound = readIO();
+
+	        if (upperBound == -1)
+	            System.out.println("Element is not a positive integer.");
+	        else
+	        	System.out.println(upperBound);
+
 		}
 	}
 	
-	public static int[] readIO()
+	public static int readIO()
 	{
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please enter a value of N.");		// Telling you what to do.
 		String n = scan.nextLine();								// Reading in what you want to scan
 		String regexCheck = "[0-9]+";							// We only want input of pure digits, so that's the first step.
 		Pattern p = Pattern.compile(regexCheck);
-		int[] nums = new int[n.length()];
 		if (p.matcher(n).matches())
-		{
-			for (int i = 0; i < n.length(); i++)
-				nums[i] = Character.getNumericValue(n.charAt(i));		// Converting all characters in the string to an array of ints for searching and comparing
-		}
-		else
-			nums = new int[0];
-		return nums;
+			return Integer.parseInt(n);
+		return -1;
 	}
 
 }
