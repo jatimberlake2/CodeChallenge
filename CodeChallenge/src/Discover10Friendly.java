c8d352a382cb67d600d55151c891ed8265e65225
Tony <jatimberlake1@crimson.ua.edu>
2020-09-24 13:07:40 -0500

    Re-factoring for printout of numeric values

--- CodeChallenge/src/Discover10Friendly.java
+++ CodeChallenge/src/Discover10Friendly.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -7,16 +8,26 @@ public class Discover10Friendly {
 	{
 		while (true)
 		{
-			Scanner scan = new Scanner(System.in);
-			System.out.println("Please enter a value of N.");		// Telling you what to do.
-			String n = scan.nextLine();								// Reading in what you want to scan
-			String regexCheck = "[0-9]+";							// We only want input of pure digits, so that's the first step.
-			Pattern p = Pattern.compile(regexCheck);
-			if (!p.matcher(n).matches())
-				System.out.println("[]\n");							// This will probably also change later.
-			else
-				System.out.println("These sure are numbers.\n");	// We will continue developing our solution from here.
+			System.out.println(Arrays.toString(readIO()));
 		}
+	}
+	
+	public static int[] readIO()
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a value of N.");		// Telling you what to do.
+		String n = scan.nextLine();								// Reading in what you want to scan
+		String regexCheck = "[0-9]+";							// We only want input of pure digits, so that's the first step.
+		Pattern p = Pattern.compile(regexCheck);
+		int[] nums = new int[n.length()];
+		if (p.matcher(n).matches())
+		{
+			for (int i = 0; i < n.length(); i++)
+				nums[i] = Character.getNumericValue(n.charAt(i));		// Converting all characters in the string to an array of ints for searching and comparing
+		}
+		else
+			nums = new int[0];
+		return nums;
 	}
 
 }
