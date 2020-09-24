8ec5807d5ec497421a98402034f426f0dc2ee5c2
Tony <jatimberlake1@crimson.ua.edu>
2020-09-24 12:45:08 -0500

    Adding some comments now that this git repo is setup properly.

--- CodeChallenge/src/Discover10Friendly.java
+++ CodeChallenge/src/Discover10Friendly.java
@@ -8,14 +8,14 @@ public class Discover10Friendly {
 		while (true)
 		{
 			Scanner scan = new Scanner(System.in);
-			System.out.println("Please enter a value of N.");
-			String n = scan.nextLine();
-			String regexCheck = "[0-9]+";
+			System.out.println("Please enter a value of N.");		// Telling you what to do.
+			String n = scan.nextLine();								// Reading in what you want to scan
+			String regexCheck = "[0-9]+";							// We only want input of pure digits, so that's the first step.
 			Pattern p = Pattern.compile(regexCheck);
 			if (!p.matcher(n).matches())
-				System.out.println("[]\n");
+				System.out.println("[]\n");							// This will probably also change later.
 			else
-				System.out.println("These sure are numbers.\n");
+				System.out.println("These sure are numbers.\n");	// We will continue developing our solution from here.
 		}
 	}
 
